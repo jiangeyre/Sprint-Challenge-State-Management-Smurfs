98d7881b108a44237bc4eba6f46b53d73518ceeb
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-12-13 08:26:31 -0800

    Commit 2: set up of index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+// reducers
+import reducer from './store/reducers/reducer';
+// components
+import SmurfVillage from './components/SmurfVillage';
+import AddSmurf from './components/AddSmurf';
+// styling
 import "./index.css";
-import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// import App from "./components/App";
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+function App(){
+    return(
+        <div>
+
+        </div>
+    )
+};
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById("root")
+);
