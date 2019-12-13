6856e7941dc5187f333ba36caf85f12747720f15
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-12-13 09:38:26 -0800

    Commit 4

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -6,13 +6,19 @@ import SmurfVillage from './SmurfVillage';
 import NewSmurf from './NewSmurf';
 
 import "./App.css";
+import sacrifice from '../img/sacrifice.jpg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{display: 'flex', flexDirection: 'column', alignContent: 'center', justifyContent: 'center'}}>
           <h1>Smurfs Available for Sacrifice:</h1>
-          <button onClick={() => {this.props.getSmurfs()}}>Get Smurfs</button>
+          <div>
+            <img src={sacrifice} alt="Ironman Tony Stark crying tears sacrifice" style={{width: 600}} />
+          </div>
+          <div>
+            <button onClick={() => {this.props.getSmurfs()}} style={{width: 120}}>Get Smurfs</button>
+          </div>
           <SmurfVillage smurfs={this.props.smurfs}/>
           <NewSmurf />
       </div>
